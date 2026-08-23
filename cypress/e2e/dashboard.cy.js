afdbd672ef3a43fae2eb37bb62edb9e65ed7df32
sachina oli <oli.sachina99@gmail.com>
2026-08-23 11:25:02 +0545

    Add dashboard

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -4,38 +4,27 @@ describe("Softix Payment Gateway - Dashboard", () => {
     cy.login();
   });
 
-
   it("TC018 - should login successfully and open dashboard", () => {
-
     cy.url()
-      .should("not.include", "/login");
-
+      .should("not.include", "/system");
   });
-
 
   it("TC019 - should display dashboard page", () => {
-
     cy.get("body")
       .should("be.visible");
-
   });
 
-
-  it("TC020 - should display navigation menu", () => {
-
-    cy.get("nav")
-      .should("be.visible");
-
+   it("TC020 - should display page content after login", () => {
+    cy.get("body")
+      .invoke("text")
+      .should("not.be.empty");
   });
-
 
   it("TC021 - should not redirect back to login after refresh", () => {
-
     cy.reload();
 
     cy.url()
-      .should("not.include", "/login");
-
+      .should("not.include", "/system");
   });
 
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,15 +3,20 @@ Cypress.Commands.add("login", () => {
   cy.visit("/system");
 
   cy.get('input[placeholder="Username/Email/Phone"]')
+    .should("be.visible")
+    .clear()
     .type(Cypress.env("username"));
 
   cy.get('input[type="password"]')
+    .should("be.visible")
+    .clear()
     .type(Cypress.env("password"));
 
-  cy.contains("Sign In")
+  cy.contains("button", "Sign In")
+    .should("be.visible")
     .click();
 
-  cy.url({ timeout: 15000 })
-    .should("not.include", "/login");
+  cy.url({ timeout: 89000 })
+    .should("not.include", "/system/login");
 
 });
